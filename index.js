c3f189bb323bffa5af628ea4edcddbc51d00f5e1
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2024-05-14 23:43:46 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -178,15 +178,16 @@
 // console.log(months.splice(0,2));
 // console.log(months)
 //reduce()
-const array1 = [1, 2, 3, 4];
+// const array1 = [1, 2, 3, 4];
 
-// 0 + 1 + 2 + 3 + 4
-const initialValue = 0;
-const sumWithInitial = array1.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  initialValue,
-);
+// // 0 + 1 + 2 + 3 + 4
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue,
+// );
 
-console.log(sumWithInitial);
+// console.log(sumWithInitial);
 // Expected output: 10
+//some()
 
